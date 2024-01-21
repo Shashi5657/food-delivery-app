c7a544c17014443fe92eede61cdd545c2dcff6ff
Shashi5657 <sangepushashidhar@gmail.com>
2024-01-21 21:08:15 +0530

    adding header styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,23 +2,24 @@ import React from "react";
 import { Container } from "reactstrap";
 import logo from "../../assets/images/res-logo.png";
 import { NavLink, Link } from "react-router-dom";
+import "../../styles/header.css";
 
 const nav__link = [
   {
     display: "Home",
-    path : '/home'
+    path: "/home",
   },
   {
     display: "Foods",
-    path : '/foods'
+    path: "/foods",
   },
   {
     display: "Cart",
-    path : '/cart'
+    path: "/cart",
   },
   {
     display: "Contact",
-    path : '/contact'
+    path: "/contact",
   },
 ];
 
@@ -26,14 +27,43 @@ const Header = () => {
   return (
     <header className="header">
       <Container>
-        <div className="nav__wrapper">
+        <div className="nav__wrapper d-flex align-items-center justify-content-between">
           <div className="logo">
             <img src={logo} alt="logo" />
             <h5>Tasty Treat</h5>
           </div>
 
           <div className="navigation">
-            <div className="menu"></div>
+            <div className="menu d-flex align-items-center gap-5">
+              {nav__link.map((item, index) => (
+                <NavLink
+                  to={item.path}
+                  key={index}
+                  className={(navClass) =>
+                    navClass.isActive ? "active__menu" : ""
+                  }
+                >
+                  {item.display}
+                </NavLink>
+              ))}
+            </div>
+          </div>
+
+          <div className="nav__right d-flex align-items-center gap-4">
+            <span className="cart__icon">
+              <i class="ri-shopping-basket-line"></i>
+              <span className="cart__badge"></span>
+            </span>
+
+            <span className="user">
+              <Link to="/login">
+                <i class="ri-user-line"></i>
+              </Link>
+            </span>
+
+            <span className="mobile__menu">
+              <i class="ri-user-line"></i>
+            </span>
           </div>
         </div>
       </Container>
